3d8412f3bec885a47a44ef0733ca7c351a607fd8
Przemysław Klejno <przemyslaw.klejno@onedynamics.pl>
2023-05-26 20:19:53 +0200

    initial lesson 1

---
+++ lekcja1-wprowadzenie.py
@@ -0,0 +1,14 @@
+# jak wygląda składnia pythona
+
+print('Hello world!') # to wyrażenie wypisze na ekranie napisa Hello world!
+
+a = 12
+b = 14
+c = a + b
+
+print(c)
+
+
+# 1. Zmienne - czym są, podstawowe operacje
+# a = 2
+# a zawiera w sobie wartość 2
